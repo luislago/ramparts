605d5d6e206ddc2b5c46b25f77e25a0222053091
Luís Lago <luisalveslago@gmail.com>
2013-09-18 12:33:49 +0100

    completed

--- rampart.rb
+++ rampart.rb
@@ -1,30 +1,29 @@
 require 'rubygems'
 require 'open-uri'
-require 'restclient'
 
 #downloads a "The Realist" issue you choose to your disk
 
 DATA_DIR = "ramparts" 
 Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
-BASE_ramparts_URL = "http://www.unz.org/Pub/Ramparts-" #this is the site where the issues are
+BASE_ramparts_URL = "http://64.62.200.70/PERIODICAL/PDF/Ramparts-" #this is the site where the issues are
 puts "year"
 year = gets.chomp
 puts "month (in english, downcase and just 3 letters)"
 month = gets.chomp
+puts "total pages"
+total_pages = gets.chomp
 issue = year + month
 ISSUE_DIR = DATA_DIR + "/" + issue
 Dir.mkdir(ISSUE_DIR) unless File.exists?(ISSUE_DIR) #creates folder to store the issue on the DATA_DIR and gives 
 #it the year and month of the issue
 puts "Creating #{ISSUE_DIR} folder"
+pagina = BASE_ramparts_URL + issue + "/1-" + total_pages + "/" #it creates a pdf starting on the first page and ending
+#on the page I set
 
 
+		open(pagina) {|f|
+File.open("#{ISSUE_DIR}/#{issue}.pdf", 'w') do |file| file.puts f.read end}
 
-		
-		for number in 1..9
-			pagina = BASE_ramparts_URL + issue + "?View=PDF&apages=000" + number.to_s 
-		pdf_file = RestClient.get(pagina)
-File.open("#{ISSUE_DIR}/#{number}.pdf", 'w'){|f| f.write(pdf_file)}
-	end
 
 puts "done"
 
