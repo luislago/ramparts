a451c2e336afade511826c226e64d11d13e6aac6
Luís Lago <luisalveslago@gmail.com>
2013-09-18 14:48:23 +0100

    some improvements

--- rampart.rb
+++ rampart.rb
@@ -10,19 +10,36 @@ puts "year"
 year = gets.chomp
 puts "month (in english, downcase and just 3 letters)"
 month = gets.chomp
-puts "total pages"
-total_pages = gets.chomp #I have to set the number of pages, so the website can create the pdf with the complete issue to
+puts "first page"
+first_page = gets.chomp
+puts "last page"
+last_page = gets.chomp
+total_pages = first_page + "-" + last_page #I have to set the number of pages, so the website can create the pdf with the complete issue to
 #to download
 issue = year + month
-puts "Creating #{issue}"
-pagina = BASE_ramparts_URL + issue + "/1-" + total_pages + "/" #it creates a pdf starting on the first page and ending
+ISSUE_DIR = DATA_DIR + "/" + issue
+Dir.mkdir(ISSUE_DIR) unless File.exists?(ISSUE_DIR)
+puts "Creating #{issue}, pages #{total_pages}"
+pagina = BASE_ramparts_URL + issue + "/" + total_pages + "/" #it creates a pdf starting on the first page and ending
 #on the page I set
 
 
 		open(pagina) {|f|
-File.open("#{DATA_DIR}/#{issue}.pdf", 'w') do |file| file.puts f.read end}  #downloads the created pdf on the 
+File.open("#{ISSUE_DIR}/#{total_pages}.pdf", 'w') do |file| file.puts f.read end}  #downloads the created pdf on the 
 	#DATA_DIR and gives it the year and month of the issue as the name
+puts "first page"
+first_page = gets.chomp
+puts "last page"
+last_page = gets.chomp
+total_pages = first_page + "-" + last_page
+puts "Creating #{issue}, pages #{total_pages}"
+pagina = BASE_ramparts_URL + issue + "/" + total_pages + "/" #it creates a pdf starting on the first page and ending
+#on the page I set
 
+
+		open(pagina) {|f|
+File.open("#{ISSUE_DIR}/#{total_pages}.pdf", 'w') do |file| file.puts f.read end}  #downloads the created pdf on the 
+	#DATA_DIR and gives it the year and month of the issue as the name
 
 
 puts "done"
